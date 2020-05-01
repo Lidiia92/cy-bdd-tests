debd2c894e94f01af8899a8e5a88899d770689a3
Lidii <lidiiany92@gmail.com>
2020-05-01 12:48:07 -0700

    added LoginPage class

--- cypress/integration/cucumber-tests/login/loginSteps.js
+++ cypress/integration/cucumber-tests/login/loginSteps.js
@@ -1,13 +1,19 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import LoginPage from './loginPage';
 
 Given('I open the login page', () => {
-	cy.visit('http://zero.webappsecurity.com/login.html');
+	// cy.visit('http://zero.webappsecurity.com/login.html');
+	LoginPage.visitLoginPage();
 });
 
 When('I submit the login form', () => {
-	cy.get('#user_login').type('username');
-	cy.get('#user_password').type('password');
-	cy.get('input[name="submit"]').click();
+	// cy.get('#user_login').type('username');
+	// cy.get('#user_password').type('password');
+	// cy.get('input[name="submit"]').click();
+
+	LoginPage.fillUsername('username');
+	LoginPage.fillPassword('password');
+	LoginPage.submit();
 });
 
 Then('I should see the home page', () => {
